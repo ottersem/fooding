0db54165400599e7a86b76596991a3268c4ed33b
Ebee1205 <spiverain02@gmail.com>
2025-11-21 18:02:41 +0900

    feat: signup

--- frontend/src/pages/Auth/SignUp.vue
+++ frontend/src/pages/Auth/SignUp.vue
@@ -1,4 +1,19 @@
 <template>
+  <v-row 
+    no-gutters
+    class="justify-center | mt-12"
+  >
+    <v-chip>
+      학교 이메일로 가입
+    </v-chip>
+  </v-row>
+  <v-row 
+    no-gutters
+    class="justify-center | mt-6"
+  >
+    <v-col cols="12" class="mt-9">비슷한 사람들과 가벼운 밥약부터 시작해요.</v-col>
+    <v-col cols="12" class="mt-9">학교 인증으로 더욱 안전하게 이용해요.</v-col>
+  </v-row>
 
 </template>
 
