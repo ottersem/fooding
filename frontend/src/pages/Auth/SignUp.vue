807e5bc170f268965ac6e36801d2f61d6b4fed4b
Ebee1205 <spiverain02@gmail.com>
2025-11-21 20:06:21 +0900

    feat: 로그인UI, 화면 전환

--- frontend/src/pages/Auth/SignUp.vue
+++ frontend/src/pages/Auth/SignUp.vue
@@ -15,28 +15,30 @@
       <v-col cols="12" class="mt-1 | header-title">비슷한 사람들과 가벼운 밥약부터 시작해요.</v-col>
       <v-col cols="12" class="mt-1 | header-subtitle">학교 인증으로 더욱 안전하게 이용해요.</v-col>
     </v-row>
-    <v-row no-gutters class="justify-center | inputbox">
+    <v-row no-gutters class="justify-center">
       <v-text-field
         v-model="Username"
         placeholder="username"
-        
+        class="inputbox"
         variant="outlined" density="comfortable" rounded="lg" bg-color="#F9FAFB" base-color="#E5E8EB" color="#E5E8EB"
       />
     </v-row>
-    <v-row no-gutters class="justify-center | inputbox">
+    <v-row no-gutters class="justify-center">
       <v-text-field
         v-model="Username"
         placeholder="email@cau.ac.kr"
+        class="inputbox"
         variant="outlined" density="comfortable" rounded="lg" bg-color="#F9FAFB" base-color="#E5E8EB" color="#E5E8EB"
       />
     </v-row>
-    <v-row no-gutters class="justify-center | inputbox">
+    <v-row no-gutters class="justify-center">
       <v-text-field
         v-model="password"
         placeholder="password"
         :type="showPassword ? 'text' : 'password'"
         :append-inner-icon="showPassword ? 'mdi-eye-outline' : 'mdi-eye-off-outline'"
         @click:append-inner="showPassword = !showPassword"
+        class="inputbox"
         variant="outlined" density="comfortable" rounded="lg" bg-color="#F9FAFB" base-color="#E5E8EB" color="#E5E8EB"
       />
     </v-row>
@@ -62,7 +64,7 @@
       <v-col
         cols="auto"
         class="info-text | underline | login-text-link"
-        @click="handleLogin"
+        @click="handleClickBtn('gotoLogin')"
       >
         로그인
       </v-col>
@@ -76,6 +78,7 @@
 import { onMounted, onUnmounted, ref, watch } from "vue";
 import { useRouter, useRoute } from "vue-router";
 import BoxContainer from "@/components/BoxContainer.vue";
+import { navigateTo } from '@/common/RouterUtil.js';
 
 const emit = defineEmits(['hide-top-appbar']);
 const router = useRouter(); 
@@ -98,6 +101,21 @@ onUnmounted(() => {
 
 // ----- 함수 정의 ----- //
 
+function handleClickBtn(action) {
+  switch (action) {
+    case 'gotoLogin':
+      navigateTo(router, '/login');
+      break;
+
+    case 'login':
+      console.log('로그인 버튼 클릭. 이메일:', email.value, '비밀번호:', password.value);
+      // 로그인 처리 로직
+      // 예: loginUser(email.value, password.value);
+      break;
+    default:
+      console.error('알 수 없는 인증 액션 타입:', action);
+  }
+}
 
 </script> 
 
