a04731ddb6601b0da29b321351c3858d4f0a19e2
Ebee1205 <spiverain02@gmail.com>
2025-11-23 15:16:56 +0900

    feat: svg loader 설정

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -10,6 +10,26 @@ import 'vuetify/styles'
 
 // Composables
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+// SVG Icons (assets/icons)
+import IconCalendar from '@/assets/icons/calender.svg?component'
+import IconClock from '@/assets/icons/clock-icon.svg?component'
+import IconLocation from '@/assets/icons/location-icon.svg?component'
+import IconPeople from '@/assets/icons/people-icon.svg?component'
+import IconShineFill from '@/assets/icons/shine-icon-fill.svg?component'
+import IconShineLine from '@/assets/icons/shine-icon-line.svg?component'
+
+const customAliases = {
+  ...aliases,
+
+  'cus-calendar': IconCalendar,
+  'cus-clock': IconClock,
+  'cus-location': IconLocation,
+  'cus-people': IconPeople,
+  'cus-shine-fill': IconShineFill,
+  'cus-shine-line': IconShineLine,
+}
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
@@ -27,4 +47,12 @@ export default createVuetify({
       }
     }
   },
+      
+  icons: {
+    defaultSet: 'mdi',
+    aliases: customAliases,
+    sets: {
+      mdi,
+    },
+  }
 })
