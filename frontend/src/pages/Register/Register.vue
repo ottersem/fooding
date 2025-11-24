a6bf09a2ff839fc36509c2080a11b60f1398f022
Ebee1205 <spiverain02@gmail.com>
2025-11-24 16:20:58 +0900

    fix: 변수명 통일, UI 통일, 버튼 활성/비활성 로직 추가

--- frontend/src/pages/Register/Register.vue
+++ frontend/src/pages/Register/Register.vue
@@ -15,25 +15,37 @@
       <v-col cols="12" class="mt-1 | header-title">비슷한 사람들과 가벼운 밥약부터 시작해요.</v-col>
       <v-col cols="12" class="mt-1 | header-subtitle">학교 인증으로 더욱 안전하게 이용해요.</v-col>
     </v-row>
-    <v-row no-gutters class="justify-center">
+
+    <v-row no-gutters class="justify-start | label-text">
+      사용자 이름
+    </v-row>
+    <v-row no-gutters class="justify-center | mt-1">
       <v-text-field
-        v-model="Username"
+        v-model="userName"
         placeholder="username"
         class="inputbox"
         variant="outlined" density="comfortable" rounded="lg" bg-color="#F9FAFB" base-color="#E5E8EB" color="#E5E8EB"
       />
     </v-row>
-    <v-row no-gutters class="justify-center">
+
+    <v-row no-gutters class="justify-start | label-text">
+      이메일
+    </v-row>
+    <v-row no-gutters class="justify-center | mt-1">
       <v-text-field
-        v-model="Username"
+        v-model="userEmail"
         placeholder="email@cau.ac.kr"
         class="inputbox"
         variant="outlined" density="comfortable" rounded="lg" bg-color="#F9FAFB" base-color="#E5E8EB" color="#E5E8EB"
       />
     </v-row>
-    <v-row no-gutters class="justify-center">
+
+    <v-row no-gutters class="justify-start | label-text">
+      비밀번호
+    </v-row>
+    <v-row no-gutters class="justify-center | mt-1">
       <v-text-field
-        v-model="password"
+        v-model="userPassword"
         placeholder="password"
         :type="showPassword ? 'text' : 'password'"
         :append-inner-icon="showPassword ? 'mdi-eye-outline' : 'mdi-eye-off-outline'"
@@ -47,11 +59,14 @@
         @click="handleClickBtn('register')"
         variant="outlined"
         class="active-btn"
+        :disabled="!active"
+        v-if="active"
       >회원가입</v-btn>
       <v-btn
         variant="outlined"
         class="readonly-btn"
         readonly
+        v-else
       >회원가입</v-btn>
     </v-row>
 
@@ -84,11 +99,11 @@ import { navigateTo } from '@/common/RouterUtil.js';
 const emit = defineEmits(['hide-top-appbar']);
 const router = useRouter(); 
 
-// v-model 변수 선언
-const username = ref('');
-const email = ref('');
-const password = ref('');
-// 비밀번호 보이기/가리기 상태 변수
+const userName = ref('');
+const userEmail = ref('');
+const userPassword = ref('');
+
+const active = ref(false);
 const showPassword = ref(false);
 
 // ----- 라이프 사이클 ----- //
@@ -100,6 +115,15 @@ onUnmounted(() => {
 
 });
 
+watch([userName, userEmail, userPassword], ([newUserName, newUserEmail, newUserPassword]) => {
+  // 세 항목 모두 null, undefined, 또는 빈 문자열이 아닐 때만 true
+  const isUsernameValid = !!newUserName && newUserName.trim() !== '';
+  const isEmailValid = !!newUserEmail && newUserEmail.trim() !== '';
+  const isPasswordValid = !!newUserPassword && newUserPassword.trim() !== '';
+  
+  active.value = isUsernameValid && isEmailValid && isPasswordValid;
+}, { immediate: true });
+
 // ----- 함수 정의 ----- //
 
 function handleClickBtn(action) {
@@ -109,8 +133,12 @@ function handleClickBtn(action) {
       break;
 
     case 'register':
-      console.log('회원가입 버튼 클릭. 이메일:', email.value, '비밀번호:', password.value);
-      navigateTo(router, '/register/basic');
+      if (active.value) {
+        console.log('회원가입 버튼 클릭. 이름:', userName.value, '이메일:', userEmail.value, '비밀번호:', userPassword.value);
+        navigateTo(router, '/register/basic');
+      } else {
+        console.log('입력 필드 미완료로 회원가입 버튼 비활성화');
+      }
       break;
 
     default:
@@ -118,7 +146,7 @@ function handleClickBtn(action) {
   }
 }
 
-</script> 
+</script>
 
 <style scoped>
 .align-item-center {
@@ -153,6 +181,11 @@ function handleClickBtn(action) {
   opacity: 1;
 }
 
+.label-text {
+  font-size: 14px;
+  font-weight: 600;
+  color: #364153;
+}
 
 .info-text {
   font-size: 16px;
